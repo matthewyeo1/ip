3d08341180a23dc76a9a8fd580639defa8d8ae5f
Matthew <e1155311@u.nus.edu>
2025-01-25 14:11:00 +0800

    Ace class: add mark, unmark, and verification functions
    * Added markAsDone() and unMark() methods to toggle task completion
      status.
    * Implemented isDone() method to verify the current completion status of
      the task.
    * Improved clarity of status management in the Task class.

--- src/main/java/Ace.java
+++ src/main/java/Ace.java
@@ -1,24 +1,22 @@
 import java.util.Scanner;
 import lib.Task;
 
-
 public class Ace {
 
     public static void setDivider() {
         System.out.println("__________________"
-            + "____________________"
-            + "____________________"
-            + "____________________"
-            + "____________________"
-            + "____________________"
-            + "____________________"
-        );
+                + "____________________"
+                + "____________________"
+                + "____________________"
+                + "____________________"
+                + "____________________"
+                + "____________________");
     }
 
     public static void setGap() {
         System.out.println("");
     }
-    
+
     // Echo function
     public static void echo(String input) {
         System.out.print(input + "\n");
@@ -37,29 +35,76 @@ public class Ace {
         System.out.println("Error: Task list is full. Could not add: " + t.getDescription());
     }
 
-    // Show list
+    // Show list function
     public static void showList(Task[] taskArray) {
         setGap();
         for (int i = 0; i < taskArray.length; i++) {
             if (taskArray[i] == null) {
                 return;
             }
-            System.out.println((i + 1) + ". " + taskArray[i].getDescription());
+            String statusIcon = taskArray[i].getStatusIcon();
+            System.out.println((i + 1) + ".[" + statusIcon + "] " + taskArray[i].getDescription());
         }
     }
 
+    // Mark task as done function
+    public static void markAsDone(Task t) {
+        t.setDone(true);
+    }
+
+    // Unmark task function
+    public static void unMark(Task t) {
+        t.setDone(false);
+    }
+
+    // Function to check if the mark command is valid
+    public static boolean isMarkCommand(String input) {
+        if (input.length() > 5 && input.substring(0, 5).equalsIgnoreCase("mark ")) {
+            String taskIdStr = input.substring(5);
+            try {
+                int taskId = Integer.parseInt(taskIdStr);
+                return (isTaskIDValid(taskId));
+            } catch (NumberFormatException e) {
+                return false;
+            }
+        }
+        return false;
+    }
+
+    // Function to check if the unmark command is valid
+    public static boolean isUnMarkCommand(String input) {
+        if (input.length() > 7 && input.substring(0, 7).equalsIgnoreCase("unmark ")) {
+            String taskIdStr = input.substring(7);
+            try {
+                int taskId = Integer.parseInt(taskIdStr);
+                return (isTaskIDValid(taskId));
+            } catch (NumberFormatException e) {
+                return false;
+            }
+        }
+        return false;
+    }
+
+    // Function to check validity of input task ID
+    public static boolean isTaskIDValid(int taskId) {
+        if (taskId < 1 || taskId > 100) {
+            System.out.println("Invalid task id: " + taskId + ". Task ID must be between 1 and 100.");
+            setGap();
+            setDivider();
+            return false;
+        }
+        return true;
+    }
+
+    // Main function
     public static void main(String[] args) {
-
-        // Create a Scanner to read user input
-        Scanner scanner = new Scanner(System.in); 
-
-        String logo =
-              " _____   _____   _____ \n"
-            + "|  _  | |  __ | |  ___|\n" 
-            + "| |_| | | |     | |___ \n"
-            + "|  _  | | |     |  ___|\n"
-            + "| | | | | |___  | |___ \n"
-            + "|_| |_| |_____| |_____|\n";
+        Scanner scanner = new Scanner(System.in); // Create a Scanner to read user input
+        String logo = " _____   _____   _____\n"
+                + "|  _  | |  __ | |  ___|\n"
+                + "| |_| | | |     | |___ \n"
+                + "|  _  | | |     |  ___|\n"
+                + "| | | | | |___  | |___\n"
+                + "|_| |_| |_____| |_____|\n";
 
         setDivider();
         setGap();
@@ -67,15 +112,14 @@ public class Ace {
         System.out.println("How can I assist you?");
         setGap();
         setDivider();
-        
-        // Initialize task array of size 100
+
+        // Initialize array for task list
         Task[] tasks = new Task[100];
 
-        // Infinite loop to perpetually scan for user input until user input == 'bye' (not case-sensitive)
         while (true) {
             setGap();
             System.out.print("> ");
-            String input=scanner.nextLine();
+            String input = scanner.nextLine();
             setGap();
 
             if (input.equalsIgnoreCase("bye")) {
@@ -87,13 +131,53 @@ public class Ace {
                 showList(tasks);
                 setGap();
                 setDivider();
-            } else {
-                setGap();
-                Task t = new Task(input);
-                addToList(tasks, t);
+            } else if (isMarkCommand(input)) {
+                int taskId = Integer.parseInt(input.substring(5)); // extract task ID
+
+                if (tasks[taskId - 1] != null) {
+                    if ((tasks[taskId - 1]).getDoneStatus() == true) {
+                        System.out.println("Task " + taskId + " is already marked.");
+                    } else {
+                        markAsDone(tasks[taskId - 1]);
+                        System.out.println("Marked task " + taskId + " as done.");
+                    }
+                } else {
+                    System.out.println("Task " + taskId + " does not exist.");
+                }
                 setGap();
                 setDivider();
-            } 
+            } else if (isUnMarkCommand(input)) {
+                int taskId = Integer.parseInt(input.substring(7)); // extract task ID
+
+                if (tasks[taskId - 1] != null) {
+                    if ((tasks[taskId - 1]).getDoneStatus() == false) {
+                        System.out.println("Task " + taskId + " is already unmarked.");
+                    } else {
+                        unMark(tasks[taskId - 1]);
+                        System.out.println("Unmarked task " + taskId + ".");
+                    }
+                } else {
+                    System.out.println("Task " + taskId + " does not exist.");
+                }
+                setGap();
+                setDivider();
+            } else if (!input.toLowerCase().startsWith("mark") && !input.toLowerCase().startsWith("unmark")) {
+                if (input.trim().isEmpty()) {
+                    // Handle empty input
+                    System.out.println("Please enter a command.");
+                } else {
+                    setGap();
+                    Task t = new Task(input);
+                    addToList(tasks, t);
+                    setGap();
+                    setDivider();
+                }
+            } else {
+                setGap();
+                System.out.println("Invalid command.");
+                setGap();
+                setDivider();
+            }
         }
         scanner.close();
     }
--- src/main/java/lib/Task.java
+++ src/main/java/lib/Task.java
@@ -4,16 +4,29 @@ public class Task {
     protected String description;
     protected boolean isDone;
 
+    // Constructor
     public Task(String description) {
         this.description = description;
         this.isDone = false;
     }
 
+    // Function to show marked/unmarked status
     public String getStatusIcon() {
-        return (isDone ? "X" : " "); // mark done task with X
+        return (isDone ? "X" : " "); 
     }
 
+    // Description getter
     public String getDescription() {
         return description;
+    }
+
+    // Status getter
+    public Boolean getDoneStatus() {
+        return isDone;
+    }
+
+    // Status setter
+    public void setDone(Boolean x) {
+        this.isDone = x;
     }
 }
