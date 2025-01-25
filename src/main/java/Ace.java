4309e22f479c044d80d2f07ef23d97306ae78216
Matthew <e1155311@u.nus.edu>
2025-01-25 11:42:29 +0800

    Main class: add function to echo user input

--- src/main/java/Ace.java
+++ src/main/java/Ace.java
@@ -1,8 +1,10 @@
+import java.util.Scanner;
+
+
 public class Ace {
 
     public static void setDivider() {
         System.out.println("__________________"
-            + "____________________"
             + "____________________"
             + "____________________"
             + "____________________"
@@ -15,25 +17,50 @@ public class Ace {
     public static void setGap() {
         System.out.println("");
     }
+    
+    // Echo()
+    public static void echo(String input) {
+        System.out.print(input + "\n");
+    }
 
     public static void main(String[] args) {
+
+        // Create a Scanner to read user input
+        Scanner scanner = new Scanner(System.in); 
+
         String logo =
-              " _____   _____   _____                          \n"
-            + "|  _  | |  __ | |  ___|                           \n" 
-            + "| |_| | | |     | |___                      \n"
-            + "|  _  | | |     |  ___|                   \n"
-            + "| | | | | |___  | |___                         \n"
-            + "|_| |_| |_____| |_____|                             \n";
+              " _____   _____   _____ \n"
+            + "|  _  | |  __ | |  ___|\n" 
+            + "| |_| | | |     | |___ \n"
+            + "|  _  | | |     |  ___|\n"
+            + "| | | | | |___  | |___ \n"
+            + "|_| |_| |_____| |_____|\n";
+
         setDivider();
         setGap();
         System.out.println("Hello! I am...\n" + logo);
         System.out.println("How can I assist you?");
         setGap();
         setDivider();
-        setGap();
-        System.out.println("Have a good day.");
-        setGap();
-        setDivider();
-        setGap();
+
+        // Infinite loop to perpetually scan for user input until user input == 'bye' (not case-sensitive)
+        while (true) {
+            setGap();
+            System.out.print("> ");
+            String input = scanner.nextLine();
+            setGap();
+
+            if (input.equalsIgnoreCase("bye")) {
+                System.out.println("Have a good day.");
+                setGap();
+                setDivider();
+                break;
+            } else {
+                echo(input);
+                setGap();
+                setDivider();
+            }
+        }
+        scanner.close();
     }
 }
