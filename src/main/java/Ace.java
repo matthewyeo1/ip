6c9e951da2d54a3cd6b0471835eec515c50742f8
Matthew <e1155311@u.nus.edu>
2025-01-25 12:35:39 +0800

    Task class: add task class
    The task class serves to initialize a new list entry when users input a
    word or sentence that is not 'bye' or 'list'.

--- src/main/java/Ace.java
+++ src/main/java/Ace.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import lib.Task;
 
 
 public class Ace {
@@ -18,9 +19,33 @@ public class Ace {
         System.out.println("");
     }
     
-    // Echo()
+    // Echo function
     public static void echo(String input) {
         System.out.print(input + "\n");
+    }
+
+    // Add-to-list function
+    public static void addToList(Task[] taskArray, Task t) {
+        for (int i = 0; i < taskArray.length; i++) {
+            if (taskArray[i] == null) {
+                taskArray[i] = t;
+                System.out.println("Added: " + t.getDescription());
+                return;
+            }
+        }
+        // If no empty slot is found
+        System.out.println("Error: Task list is full. Could not add: " + t.getDescription());
+    }
+
+    // Show list
+    public static void showList(Task[] taskArray) {
+        setGap();
+        for (int i = 0; i < taskArray.length; i++) {
+            if (taskArray[i] == null) {
+                return;
+            }
+            System.out.println((i + 1) + ". " + taskArray[i].getDescription());
+        }
     }
 
     public static void main(String[] args) {
@@ -42,12 +67,15 @@ public class Ace {
         System.out.println("How can I assist you?");
         setGap();
         setDivider();
+        
+        // Initialize task array of size 100
+        Task[] tasks = new Task[100];
 
         // Infinite loop to perpetually scan for user input until user input == 'bye' (not case-sensitive)
         while (true) {
             setGap();
             System.out.print("> ");
-            String input = scanner.nextLine();
+            String input=scanner.nextLine();
             setGap();
 
             if (input.equalsIgnoreCase("bye")) {
@@ -55,11 +83,17 @@ public class Ace {
                 setGap();
                 setDivider();
                 break;
-            } else {
-                echo(input);
+            } else if (input.equalsIgnoreCase("list")) {
+                showList(tasks);
                 setGap();
                 setDivider();
-            }
+            } else {
+                setGap();
+                Task t = new Task(input);
+                addToList(tasks, t);
+                setGap();
+                setDivider();
+            } 
         }
         scanner.close();
     }
